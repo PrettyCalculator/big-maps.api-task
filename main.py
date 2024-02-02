eca2d68d63767e7f693b630ac43569576be16768
курс2 <karterr.r@yandex.ru>
2024-02-02 17:42:43 +0500

    2.1

--- main.py
+++ main.py
@@ -4,20 +4,16 @@ import sys
 import pygame
 import requests
 
-map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593&spn=0.001," f"{0.002}" + "&l=map"
+num = 0.002
+map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593&spn=0.001," f"{num}" + "&l=map"
 response = requests.get(map_request)
-num = 0
-n = 0.002
 
 
 def functions(n):
-    if n > num:
-        n += 0.001
-        print(n)
-    else:
-        n -= 0.001
-    print(n)
-    map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593& +spn=0.001," + f"{n}" + "&l=map"
+    global num
+    num -= 0.001
+    print(num)
+    map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593&spn=0.001," f"{num}" + "&l=map"
     response = requests.get(map_request)
     map_file = "map.png"
     with open(map_file, "wb") as file:
@@ -43,6 +39,7 @@ screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 running = True
 while running:
+    num = 0.002
     for event in pygame.event.get():
         keys = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
