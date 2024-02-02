0e3de132aa034c564de688a5f2dd22970b2930ad
курс2 <karterr.r@yandex.ru>
2024-02-02 18:03:00 +0500

    2.2

--- main.py
+++ main.py
@@ -4,16 +4,27 @@ import sys
 import pygame
 import requests
 
-num = 0.002
-map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593&spn=0.001," f"{num}" + "&l=map"
-response = requests.get(map_request)
 
 
-def functions(n):
-    global num
-    num -= 0.001
-    print(num)
-    map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593&spn=0.001," f"{num}" + "&l=map"
+def get_image():
+    map_file = 'map.png'
+    resp = requests.get(api_server, map_params)
+    with open(map_file, "wb") as file:
+        file.write(resp.content)
+    img = pygame.image.load('map.png')
+    os.remove('map.png')
+    return img
+
+
+def move_up():
+    print('да')
+    long = map_params['ll'].split()[0]
+    lat = str(float(map_params['ll'].split(',')[1]) + float(map_params['spn'].split(',')[1]))
+    map_params['ll'] = ','.join([long, lat])
+
+
+def big(n):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593&spn=" + f"{n}" + "," + f"{n}" + "&l=map"
     response = requests.get(map_request)
     map_file = "map.png"
     with open(map_file, "wb") as file:
@@ -21,31 +32,55 @@ def functions(n):
     screen = pygame.display.set_mode((600, 450))
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
+    print(n)
+    if n > 0:
+        return n
+    return 0
 
+def small(n):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593&spn=" + f"{n}" + "," + f"{n}" + "&l=map"
+    response = requests.get(map_request)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    print(n)
+    if n > 0:
+        return n
+    return 0
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
+map_params = {
+    'll': '56.049898,53.449593',
+    'spn': '0.001,0.002',
+    'l': 'map'
+}
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+api_server = "http://static-maps.yandex.ru/1.x/?"
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
+image = get_image()
+screen.blit(image, (0, 0))
 pygame.display.flip()
 running = True
+a = 0.002
 while running:
-    num = 0.002
+    num = 0.001
     for event in pygame.event.get():
         keys = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             running = False
         if keys[pygame.K_PAGEUP]:
-            functions(1)
+            num = a - 0.001
+            a = big(num)
+            image = get_image()
+        if keys[pygame.K_PAGEDOWN]:
+            print(21)
+            num = a + 0.01
+            a = big(num)
+            image = get_image()
+    screen.blit(image, (0, 0))
 
 pygame.quit()
-os.remove(map_file)
