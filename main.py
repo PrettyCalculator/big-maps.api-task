7340677941d79cd60e883eb609ee8f644e6ea50b
курс2 <karterr.r@yandex.ru>
2024-02-02 16:52:39 +0500

    1

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+import pygame
+import requests
+
+map_request = "http://static-maps.yandex.ru/1.x/?ll=56.049898%2C53.449593&spn=0.001,0.002&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
